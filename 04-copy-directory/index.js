d84e2948810710f62c787ef7aa9ecbadecd1894e
Max Teclav <maks.tetslav@yandex.ru>
2021-11-04 19:33:54 +0300

    feat: implement 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fsPromises = require('fs').promises;
+const fs = require('fs');
+const path = require('path');
+
+const existDirPath = path.join(__dirname, 'files/');
+const newDirPath = path.join(__dirname, 'files-copy/');
+
+fsPromises.mkdir(newDirPath, { recursive: true });
+
+fs.readdir(newDirPath, {}, (err, files) => {
+	if (err) console.log(err);
+	if (files.length) {
+		files.forEach(file => fsPromises.rm(newDirPath + file));
+	};
+});
+
+function add() {
+	fs.readdir(existDirPath, { withFileTypes: true }, (err, files) => {
+		if (err) console.log(err);
+
+		files.forEach(file => fsPromises.copyFile(existDirPath + file.name, newDirPath + file.name));
+	})
+}
+
+setTimeout(add, 1000);
 
